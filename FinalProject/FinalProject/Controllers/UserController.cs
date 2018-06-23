10fc604000975f5257d55a14a9d2f6478d224d06
Unknown <nfulper@gmail.com>
2018-06-23 14:14:50 -0400

    Made a User controller.

---
+++ FinalProject/FinalProject/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+
+    public class UserController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            List<Users> users = new List<Users>();
+            return Users;
+        }
+    }
+}
